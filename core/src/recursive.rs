ff3472da3c6f4e5a9ff936d0c9125bbea44a5854
Afsal Thaj <afsal.taj06@gmail.com>
2026-01-04 12:12:32 +1100

    add non recursive struct

---
+++ core/src/recursive.rs
@@ -0,0 +1,49 @@
+pub trait ArenaMember<Arena> {
+    type NonRecursive;
+    fn deflate(&self, arena: &mut Arena) -> Self::NonRecursive;
+    fn inflate(non_rec: Self::NonRecursive, arena: &Arena) -> Self;
+}
+
+impl<A, T: ArenaMember<A>> ArenaMember<A> for Box<T> {
+    type NonRecursive = T::NonRecursive;
+    fn deflate(&self, arena: &mut A) -> Self::NonRecursive {
+        self.as_ref().deflate(arena)
+    }
+    fn inflate(val: Self::NonRecursive, arena: &A) -> Self {
+        Box::new(T::inflate(val, arena))
+    }
+}
+
+impl<A, T: ArenaMember<A>> ArenaMember<A> for Option<T> {
+    type NonRecursive = Option<T::NonRecursive>;
+    fn deflate(&self, arena: &mut A) -> Self::NonRecursive {
+        self.as_ref().map(|inner| inner.deflate(arena))
+    }
+    fn inflate(val: Self::NonRecursive, arena: &A) -> Self {
+        val.map(|inner| T::inflate(inner, arena))
+    }
+}
+
+impl<A, T: ArenaMember<A>> ArenaMember<A> for Vec<T> {
+    type NonRecursive = Vec<T::NonRecursive>;
+    fn deflate(&self, arena: &mut A) -> Self::NonRecursive {
+        self.iter().map(|item| item.deflate(arena)).collect()
+    }
+    fn inflate(val: Self::NonRecursive, arena: &A) -> Self {
+        val.into_iter().map(|item| T::inflate(item, arena)).collect()
+    }
+}
+
+macro_rules! impl_arena_leaf {
+    ($($t:ty),*) => {
+        $(
+            impl<A> ArenaMember<A> for $t {
+                type NonRecursive = $t;
+                fn deflate(&self, _: &mut A) -> Self::NonRecursive { self.clone() }
+                fn inflate(val: Self::NonRecursive, _: &A) -> Self { val }
+            }
+        )*
+    };
+}
+
+impl_arena_leaf!(i8, u8, i16, u16, i32, u32, i64, u64, f32, f64, String, bool, usize);
